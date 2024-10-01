aba1edf87d01d8670e504bb5ff70388ee2db28c5
Yojhantaborda17 <71045110+Yojhantaborda17@users.noreply.github.com>
2024-09-30 21:00:49 -0500

    edición

--- src/main/java/com/example/demo/Usuarios.java
+++ src/main/java/com/example/demo/Usuarios.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -11,13 +12,9 @@ import lombok.Data;
 @Table(name = "Usuarios")
 public class Usuarios {
     @Id
-    @Column(name= "ID")
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     private String nombre;
-
-
+    private String apellido;
 }
-
-
-
